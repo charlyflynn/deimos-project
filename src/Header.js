09d11ec7422ee414690d7f9aaba74185a293a290
Charly Flynn <35696585+charlyflynn@users.noreply.github.com>
2021-07-17 22:43:58 +0100

    add some vague styling

--- src/Header.js
+++ src/Header.js
@@ -3,15 +3,25 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.header`
   display: flex;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 1;
   justify-content: space-around;
+
+  a {
+    font-size: 36px;
+    text-decoration: none;
+    color: lightsteelblue;
+  }
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Link to="">Home</Link>
-      <Link to="data">Data</Link>
-      <Link to="info">Info</Link>
+      <Link to="">home</Link>
+      <Link to="data">data</Link>
+      <Link to="info">info</Link>
     </StyledHeader>
   );
 };
